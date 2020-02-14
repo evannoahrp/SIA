160b26de5784f1ba121bffdd0b2a9e42a613e1d3
Evannoah <42636732+evannoahrp@users.noreply.github.com>
2020-02-14 10:53:48 +0700

    third commit

---
+++ app/src/main/java/com/example/sia/Splash.java
@@ -0,0 +1,38 @@
+package com.example.sia;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        imageView.startAnimation(animation);
+
+        Thread timer = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),Login.class);
+                    startActivity(intent);
+                    finish();
+                    super.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        timer.start();
+    }
+}
